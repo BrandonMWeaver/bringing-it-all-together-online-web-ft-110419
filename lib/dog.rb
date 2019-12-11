7fba25a1b51fd9e41773c07c2eed8cf429d29ec5
Brandon Weaver <a7xncob@gmail.com>
2019-12-11 22:52:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   
   attr_accessor :id, :name, :breed
   
-  def initialize(attributes)
+  def initialize(id=nil, attributes)
     attributes.each { |key, value| self.send("#{key}=", value) }
   end
   
